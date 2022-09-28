ac01fcc1dd939c23e75e0bc5c4d5bd739e6ef640
lucaswatanuki <lucaswatanuki@gmail.com>
2022-09-28 19:16:08 -0300

    CAL-3 Logging requests and responses

--- src/main/java/com/installments/calculator/client/ClientConfiguration.java
+++ src/main/java/com/installments/calculator/client/ClientConfiguration.java
@@ -1,9 +1,11 @@
 package com.installments.calculator.client;
 
+import feign.Logger;
 import feign.codec.Encoder;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.cloud.openfeign.support.SpringEncoder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -15,5 +17,10 @@ public class ClientConfiguration {
         HttpMessageConverter<Object> jacksonConverter = new MappingJackson2HttpMessageConverter();
         ObjectFactory<HttpMessageConverters> objectFactory = () -> new HttpMessageConverters(jacksonConverter);
         return new SpringEncoder(objectFactory);
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
